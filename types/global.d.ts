789fd646358c8bdda475e0f1131c301046bbc767
Adam Scott <adscott@hubspot.com>
2023-12-12 12:20:20 -0500

    d12 p1

--- types/global.d.ts
+++ types/global.d.ts
@@ -11,14 +11,14 @@ interface Array<T> {
     callbackfn: (value: T, index: number, array: T[]) => void,
     thisArg?: any
   ): T[];
-  // ptAt(pt: Pt2d): T extends Array<U> ? U : never;
-  // setAt(pt: Pt2d, value: T extends Array<U> ? U : never);
+  ptAt(pt: Pt2d): T extends Array<U> ? U : never;
+  setAt(pt: Pt2d, value: T extends Array<U> ? U : never);
 }
 
-interface Array<T extends Array<U>> {
-  ptAt(pt: Pt2d): U | undefined;
-  setAt(pt: Pt2d, value: U);
-}
+// interface Array<T extends Array<U>> {
+//   ptAt(pt: Pt2d): U | undefined;
+//   setAt(pt: Pt2d, value: U);
+// }
 // interface Array<T> {
 //     peek(callbackfn: (value: T, index: number, array: T[]) => void, thisArg?: any): T[];
 //     ptAt(pt: Pt2d): T extends Array<U> ? U : never;
