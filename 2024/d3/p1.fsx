8c1905ee8c240aba642cda9202252e1f7af04255
Adam Scott <scottadam23@gmail.com>
2024-12-03 09:30:19 -0500

    d3 done

---
+++ 2024/d3/p1.fsx
@@ -0,0 +1,26 @@
+﻿#load "../utils/util.fs"
+
+open System.IO
+open System.Text.RegularExpressions
+open utils
+open System
+
+let args = fsi.CommandLineArgs
+
+let lines =
+    File.ReadAllLines args.[1]
+    |> Array.map (fun line -> Regex.Matches(line, "mul\((\d+),(\d+)\)"))
+    |> Array.map (fun matches -> matches |> Seq.cast<Match> |> Array.ofSeq)
+
+// lines
+// |> Array.iter (fun matches -> matches |> Array.iter (fun m -> printfn "Match: %A Groups: %A" m m.Groups))
+
+let result =
+    lines
+    |> Array.map (fun matches ->
+        matches
+        |> Array.map (fun m -> (Int32.Parse(m.Groups.[1].Value), Int32.Parse(m.Groups.[2].Value))))
+    |> Array.map (fun line -> line |> Array.map (fun (a, b) -> a * b) |> Array.sum)
+    |> Array.sum
+
+printfn "%d" result
---
+++ 2024/d3/p2.fsx
@@ -0,0 +1,34 @@
+﻿#load "../utils/util.fs"
+
+open System.IO
+open System.Text.RegularExpressions
+open utils
+open System
+
+let args = fsi.CommandLineArgs
+
+let data =
+    File.ReadAllLines args.[1]
+    |> Array.map (fun line -> Regex.Matches(line, "mul\((\d+),(\d+)\)|do\(\)|don't\(\)"))
+    |> Array.map (fun matches -> matches |> Seq.cast<Match> |> Array.ofSeq)
+    |> Array.concat
+
+// lines
+// |> Array.iter (fun matches -> matches |> Array.iter (fun m -> printfn "Match: %A Groups: %A" m m.Groups))
+
+let result =
+    data
+    |> Array.fold
+        (fun (op, value) curr ->
+            match curr.Groups.[0].Value with
+            | "do()" -> (true, value)
+            | "don't()" -> (false, value)
+            | _ ->
+                // printfn "Match: %A Groups: %A" curr curr.Groups
+                let a = Int32.Parse(curr.Groups.[1].Value)
+                let b = Int32.Parse(curr.Groups.[2].Value)
+
+                (if op then (true, value + a * b) else (false, value)))
+        (true, 0)
+
+printfn "%d" (snd result)
