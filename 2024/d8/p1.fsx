a0a94e5fedbc04b961dbd4faffc42f9083d63199
Adam Scott <scottadam23@gmail.com>
2024-12-09 14:56:56 -0500

    I don't understand d8

---
+++ 2024/d8/p1.fsx
@@ -0,0 +1,128 @@
+﻿#load "../utils/util.fs"
+#load "../utils/multimap.fs"
+
+open System
+open System.IO
+open System.Text.RegularExpressions
+open utils
+open multimap
+
+
+let args = fsi.CommandLineArgs
+
+let lines =
+    args.[1] |> File.ReadAllLines |> Array.map (fun line -> line.ToCharArray())
+
+type Pos = (int * int)
+type Grid = char array array
+
+type Dir =
+    | Right
+    | DLeft
+    | DRight
+    | Down
+    | Left
+    | ULeft
+    | URight
+    | Up
+
+let getOppositeDir (dir: Dir) =
+    match dir with
+    | Right -> Left
+    | DLeft -> URight
+    | DRight -> ULeft
+    | Down -> Up
+    | Left -> Right
+    | ULeft -> DRight
+    | URight -> DLeft
+    | Up -> Down
+
+let outOfBounds ((x, y): Pos, grid: Grid) =
+    not (x >= 0 && x < grid.[0].Length && y >= 0 && y < grid.Length)
+
+let step' ((x, y): Pos, dir: Dir, size: int) =
+    match dir with
+    | Right -> (x + size, y)
+    | DLeft -> (x - size, y + size)
+    | DRight -> (x + size, y + size)
+    | Down -> (x, y + size)
+    | Left -> (x - size, y)
+    | ULeft -> (x - size, y - size)
+    | URight -> (x + size, y - size)
+    | Up -> (x, y - size)
+
+let step (pos: Pos, dir: Dir) = step' (pos, dir, 1)
+
+let rec countDistanceToSameNode ((x, y): Pos, c: char, dir: Dir, grid: Grid) =
+    if outOfBounds ((x, y), grid) then
+        None
+    else if grid.[y].[x] = c then
+        Some(1)
+    else
+        match countDistanceToSameNode ((step ((x, y), dir)), c, dir, grid) with
+        | Some(value) -> Some(1 + value)
+        | None -> None
+
+
+let genAntiNodes ((x, y): Pos, grid: Grid) =
+    [| Right; DLeft; DRight; Down |]
+    |> Seq.map (fun dir -> (countDistanceToSameNode (step ((x, y), dir), grid.[y].[x], dir, grid), dir))
+    // |> Seq.map (fun v ->
+    //     printfn "%A %A %A" v y x
+    //     v)
+    |> Seq.map (fun (dist, dir) ->
+        match dist with
+        | None -> None
+        | Some(value) -> Some(step' ((x, y), dir, value * 3), step' ((x, y), getOppositeDir (dir), value * 2)))
+    |> Seq.choose id
+    |> Seq.collect (fun (a, b) ->
+        seq {
+            a
+            b
+        })
+    |> Set.ofSeq
+
+
+let nodes =
+    lines
+    |> Seq.mapi (fun y line ->
+        line
+        |> Seq.mapi (fun x c ->
+            match c with
+            | '.' -> None
+            | _ -> Some(x, y))
+        |> Seq.choose id)
+    |> Seq.collect id
+    |> Set.ofSeq
+
+// let result = genAntiNodes ((8, 1), lines)
+let result =
+    nodes
+    |> Seq.map (fun pos -> genAntiNodes (pos, lines))
+    |> Seq.collect id
+    |> Seq.filter (fun pos -> not (outOfBounds (pos, lines) || nodes.Contains(pos)))
+    |> Set.ofSeq
+
+let z = genAntiNodes ((8, 1), lines)
+
+let printGridWithLabels (grid: Grid, markedPositions: Set<Pos>) =
+    printf "  "
+
+    for x in 0 .. grid.[0].Length - 1 do
+        printf " %d " (x % 10)
+
+    printfn ""
+
+    for y in 0 .. grid.Length - 1 do
+        printf "%d " (y % 10)
+
+        for x in 0 .. grid.[y].Length - 1 do
+            if markedPositions.Contains((x, y)) then
+                printf " # "
+            else
+                printf " %c " grid.[y].[x]
+
+        printfn ""
+
+printGridWithLabels (lines, result)
+printfn "%A" result.Count
---
+++ 2024/d8/p2.fsx
@@ -0,0 +1,64 @@
+﻿#load "../utils/util.fs"
+#load "../utils/multimap.fs"
+
+open System
+open System.IO
+open System.Text.RegularExpressions
+open utils
+open multimap
+
+
+let args = fsi.CommandLineArgs
+
+let lines =
+    args.[1]
+    |> File.ReadAllLines
+    |> Array.map (fun line -> line.Split(":"))
+    |> Array.map (fun line ->
+        (int64 line.[0], line.[1].Trim().Split(" ") |> Array.map (fun num -> int64 num) |> List.ofArray))
+
+type Op =
+    | Add
+    | Mul
+    | Con
+
+let evaluate (a: int64, b: int64, op: Op) =
+    match op with
+    | Add -> a + b
+    | Mul -> a * b
+    | Con -> int64 (a.ToString() + b.ToString())
+
+let rec genSolutions' (nums: int64 list, op: Op, acc: int64, goal: int64) =
+    // printfn "%A %A %A %A" nums op acc goal
+
+    if acc > goal then
+        []
+    else
+
+        match nums with
+        | [] -> [ acc ]
+        | head :: tail ->
+            genSolutions' (tail, Add, evaluate (acc, head, op), goal)
+            |> List.append (genSolutions' (tail, Mul, evaluate (acc, head, op), goal))
+            |> List.append (genSolutions' (tail, Con, evaluate (acc, head, op), goal))
+
+
+let rec genSolutions (nums: int64 list, goal: int64) =
+    match nums with
+    | [] -> raise (Exception("bad state"))
+    | head :: tail ->
+        genSolutions' (tail, Add, head, goal)
+        |> List.append (genSolutions' (tail, Mul, head, goal))
+        |> List.append (genSolutions' (tail, Con, head, goal))
+
+let countSolutions (goal: int64, nums: int64 list) =
+    genSolutions (nums, goal) |> Seq.filter (fun x -> x = goal) |> Seq.length
+
+let result =
+    lines
+    |> Seq.map (fun (goal, nums) -> countSolutions (goal, nums), goal)
+    |> Seq.map (fun (solutions, goal) -> if solutions > 0 then goal else 0)
+    |> Seq.sum
+
+
+printfn "%A" result
